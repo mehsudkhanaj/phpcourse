21502768e6ae5c230d307559817e8e540d1f9ef4
jamshed <jamshed2019@namal.edu.pk>
2023-11-15 12:34:45 +0500

    switch and comparison

---
+++ ternaryoperator.php
@@ -0,0 +1,10 @@
+<?php 
+echo "___________________Ternary operator___________________________ <br>";
+
+$x=10;
+// ($x>15)? $z="Greater":$z="Smaller";
+$z="value is : ".($x>15?$z="Great":"Smaller");
+echo $z;
+
+
+?>
