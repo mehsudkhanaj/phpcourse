eae81d2ec86f62ff885c6a616f105264425c0613
jamshed <jamshed2019@namal.edu.pk>
2023-11-15 11:04:59 +0500

    Php new course

---
+++ comments.php
@@ -0,0 +1,36 @@
+<?php
+
+echo "___________________Comments___________________________ <br>";
+$x="Muhammad Jamshed";
+echo  "<br>" . var_dump($x);
+echo $x;
+echo  "<br>" . var_dump($x);
+echo $x;
+echo  "<br>" . var_dump($x);
+echo $x;
+// echo  "<br>" . var_dump($x);
+// echo $x;
+// echo  "<br>" . var_dump($x);
+// echo $x;
+// echo  "<br>" . var_dump($x);
+// echo $x;
+// echo  "<br>" . var_dump($x);
+// echo $x;
+// echo  "<br>" . var_dump($x);
+// echo $x;
+// echo  "<br>" . var_dump($x);
+// echo $x;
+// echo  "<br>" . var_dump($x);
+// echo $x;
+// echo  "<br>" . var_dump($x);
+// echo $x;
+// echo  "<br>" . var_dump($x);
+/*
+echo $x;
+echo  "<br>" . var_dump($x);
+echo $x;
+echo  "<br>" . var_dump($x);
+echo $x;*/
+
+
+?>
---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+echo "______________________________Variables Types__________________________________ <br>";
+$x="Muhammad Jamshed";
+echo  "<br>" . var_dump($x);
+echo $x;
+echo  "<br>" . var_dump($x);
+echo $x;
+echo  "<br>" . var_dump($x);
+echo $x;
+// echo  "<br>" . var_dump($x);
+// echo $x;
+// echo  "<br>" . var_dump($x);
+// echo $x;
+// echo  "<br>" . var_dump($x);
+// echo $x;
+// echo  "<br>" . var_dump($x);
+// echo $x;
+// echo  "<br>" . var_dump($x);
+// echo $x;
+// echo  "<br>" . var_dump($x);
+// echo $x;
+// echo  "<br>" . var_dump($x);
+// echo $x;
+// echo  "<br>" . var_dump($x);
+// echo $x;
+// echo  "<br>" . var_dump($x);
+/*
+echo $x;
+echo  "<br>" . var_dump($x);
+echo $x;
+echo  "<br>" . var_dump($x);
+echo $x;*/
+
+
+?>
