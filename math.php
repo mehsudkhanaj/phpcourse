34f84f2344d3a49421b3ec02e8b9037739b83e81
jamshed <jamshed2019@namal.edu.pk>
2023-11-15 11:21:18 +0500

    math

---
+++ math.php
@@ -0,0 +1,18 @@
+<?php 
+echo "___________________Arithematic Operator___________________________ <br>";
+$a=10;
+$b=3;
+$a++;
+$add=$a+$b;
+echo $add ,"<br>";
+$sub=$a-$b;
+echo $sub ,"<br>";
+$mul=$a*$b;
+echo $mul ,"<br>";
+$div=$a/$b ;
+echo $div ,"<br>" ;
+$power=$a**$b;
+echo $power ,"<br>";
+$mod=$a%$b ;
+echo $mod,"<br>" ;
+?>
