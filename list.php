c38751f95086f896691fe9d8bcdf7f7111c120b2
jamshed <jamshed2019@namal.edu.pk>
2023-11-21 17:32:16 +0500

    meesage

---
+++ list.php
@@ -0,0 +1,193 @@
+<?php 
+echo "__________List____________<br>";
+
+// $emp=[
+//         [1,"Muhammad Jamshed","Manager",50000],
+//         [2,"shanza","HR",90000],
+//         [3,"Muhammad Ali","Team lead",150000],
+//         [4,"Muhammad Tariq","Developer",250000],];
+
+// echo "<table border=2px  cellspacing=0px cellpadding=5px>
+// <tr><th>ID</th><th>name</th><th>designation</th><th>salary</th></tr>
+// ";
+// foreach($emp as list($id,$name,$designation,$salary)){
+//     echo "<tr><td> $id</td> <td> $name</td> <td>$designation</td>  <td>$salary</td></tr>";
+// }
+// echo "</table>";
+
+
+$smart=[
+    [1,"Muhammad Jamshed","Manager",50000],
+    [2,"shanza","HR",90000],
+    [3,"Muhammad Ali","Team lead",150000],
+    [4,"Muhammad Tariq","Developer",25000000],];
+
+    // echo "<table border=2px> 
+    // <tr>
+    // <th>ID</th>
+    // <th>Name</th>
+    // <th>Designation</th>
+    // <th>Salary</th></tr>";
+// foreach($smart as list($id,$name,$designation,$salary)){
+//     echo "<tr><td>$id</td><td>$name</td><td>$designation</td><td>$salary</td></tr>";
+// }
+// echo "</table>";
+
+
+
+// $arr=array("jamshed","Shanza","Nama","Rehang","everyone");
+// foreach($arr as $key){
+//     echo $key."<br/>";
+// }
+// $food=array('orange','orange','banana',55,'apple','pineapple');
+// $food=array(
+//     'fruits' =>array('apple','orange','banana','pineapple'),
+//     'veggie'=>array('carrot','collard','pea','bringle')
+
+
+
+
+// );
+// echo count($food['fruits'],1);
+// echo "<br>";
+// echo sizeof($food['veggie'],1);
+
+// $len=count($food); 
+// for($i=0;$i<$len;$i++){
+//     echo $food[$i]. "<br>";
+
+// }
+// echo "<pre>";
+// print_r( array_count_values($food));
+// echo "</pre>";
+
+
+// echo "____________Array Search Function_______________<br>";
+// echo in_array("lime",$food);
+// echo in_array("orange",$food);
+// if(in_array('orange',$food)){
+//     echo "find successfully";
+// }else{
+//     echo "cant find";
+// }
+// if(in_array(55,$food,true)){
+//     echo "find successfully";
+// }else{
+//     echo "cant find";
+// }
+
+// $a=array(array('php','python'),array('nodejs','javascript'),'node');
+// if(in_array(array('php','python'), $a,true)){
+//     echo 'find done';
+// }else{
+//     echo 'not found';
+// }
+// $fooda=array(
+//     'fruit'=>['orange','apple','cake'],
+//     'bakeri'=>['biscuits','jalei','sana']
+
+// );
+// $foodb=array(
+//     'a'=>"orange",
+//     'b'=>"apple",
+//     'c'=>"banana",
+//     'd'=>"sana",
+
+// );
+// echo array_search('orange',$foodb);
+// echo "____________Array Sreplace Function_______________<br>";
+
+// $food=array('orange','orange','banana',55,'apple','pineapple');
+// $food=array('orange','orange','a'=>'banana',55,'apple','pineapple');
+// $veggie=['a'=>'carrot','b'=>'pea'];
+// $colors=['red','green','yellow'];
+// $newarray=array_replace($food,$veggie,); 
+// echo "<pre>";
+// print_r($food);
+// echo "</pre>";
+// echo "<pre>";
+// print_r($newarray);
+// echo "</pre>"
+// $array1=array('a'=>array('red'),'b'=>array('1'=>'green','pink','khan'));
+// $array2=array('a'=>array('yellow'),'b'=>array('black','1'=>'purple','red'));
+// $newarray=array_replace_recursive($array1,$array2);
+// echo "<pre>";
+// print_r($newarray);
+// echo "</pre>";
+// echo "____________Array Push POP Function_______________<br>";
+// $foodb=array(
+//     'a'=>"orange",
+//     'b'=>"apple",
+//     'c'=>"banana",
+//     'd'=>"sana",
+
+// );
+// $food=array('asad','orange','banana',55,'apple','pineapple');
+// $food2=array('Tariq','sangtra','kela',53,'shafi','jimi');
+// array_pop($food);
+// array_push($food,'jimifruit','sana');
+// echo "____________shift and unshift Function_______________<br>";
+// array_shift($food);
+// array_unshift($food,'chiko','asad','malaika');
+
+// echo "<pre>";
+// print_r($food);
+// echo "</pre>";
+// echo "____________Array Merge and Array Combine_______________<br>";
+// $newarray=array_merge($foodb,$food2);
+// echo "<pre>";
+// print_r($newarray);
+// echo "</pre>";
+
+echo "____________Array Splice Function_______________<br>";
+// $food=array('asad','orange','banana',55,'apple','pineapple');
+// $newarray=array_slice($food,0,4);
+// $foodb=array(
+//     'a'=>"orange",
+//     'b'=>"apple",
+//     '34'=>"banana",
+//     'd'=>"sana",
+
+// );
+// $newarray=array_slice($foodb,2,5,true);
+// echo "<pre>";
+// print_r($newarray);
+// echo "</pre>";
+echo "____________Array Splice Function_______________<br>";
+
+
+
+$food=array('orange','asad','banana',55,'apple','pineapple');
+$veggie=['red','yellow'];
+array_splice($food,1,2,$veggie);
+// array_splice($food,2);
+echo "<pre>";
+print_r($food);
+echo "</pre>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
