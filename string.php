a7f8c4b8d3c9c86ba9843d2aa2e1f6eed9af63e1
jamshed <jamshed2019@namal.edu.pk>
2023-11-25 11:10:28 +0500

    string slash

--- string.php
+++ string.php
@@ -1,25 +1,41 @@
 <?php
 echo "___________String_________<br>";
-$str="I love php and we love python also";
-$new=str_ireplace("PHP","nodejs",$str);
-echo "<pre>";
-echo $new;
-echo "</pre>";
-$newa=substr_replace($str,"python and laravel",0,10);
-echo "<pre>";
-echo $newa;
-echo "</pre>";
-$strrr=strtr($str,"ov","hello");
-echo "<pre>";
-echo $strrr;
-echo "</pre>";
-$rev=strrev($str);
-$shuf=str_shuffle($str);
-echo "<pre>";
-echo $shuf;
-echo "</pre>";
-echo str_pad($str,100,"........",STR_PAD_BOTH);
+$str="Hello I am 'Muhammad Jamshed'";
+// $new=str_ireplace("PHP","nodejs",$str);
+// echo "<pre>";
+// echo $new;
+// echo "</pre>";
+// $newa=substr_replace($str,"python and laravel",0,10);
+// echo "<pre>";
+// echo $newa;
+// echo "</pre>";
+// $strrr=strtr($str,"ov","hello");
+// echo "<pre>";
+// echo $strrr;
+// echo "</pre>";
+// $rev=strrev($str);
+// $shuf=str_shuffle($str);
+// echo "<pre>";
+// echo $shuf;
+// echo "</pre>";
+// echo trim($str,);
+// echo str_pad($str,100,"........",STR_PAD_BOTH);
 // echo substr($str,-3);
+
+echo "<pre>";
+$newslash=addslashes($str);
+echo $newslash;
+echo "____<br>";
+$newstr=stripslashes($newslash);
+echo $newstr;
+echo "</pre>";
+echo "____<br>";
+// $newstr=addcslashes($str,"Jam");
+$newstr=addcslashes($str,"a..z");
+echo $newstr;
+
+
+
 
 
 ?>
